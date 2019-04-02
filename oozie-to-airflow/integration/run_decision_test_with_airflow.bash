aa05680d2f42bfa0f31193306ae4f36a45445d15
Googler <noreply@google.com>
2019-04-02 14:11:20 +0200

    Finalize the decision control node mapper
    - Fix the template (returning task ID - as required - instead of task object)
    - Add basic EL function conversion in the mapper
    - Add an example showcasing the conversion of a basic EL function
    - Add integration test
    
    GitOrigin-RevId: b372d844415035554e1ef931e9c13783c82c20d5
    Change-Id: I872022cb5899871fb6f9659fa765d3efcdcbcd97

---
+++ oozie-to-airflow/integration/run_decision_test_with_airflow.bash
@@ -0,0 +1,36 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#!/usr/bin/env bash
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR=${MY_DIR}/..
+
+if [[ ! -f ${BASE_DIR}/examples/decision/configuration.properties ]]; then
+    echo
+    echo "Please copy ${BASE_DIR}/examples/decision/configuration-template.properties to ${BASE_DIR}/examples/decision/configuration.properties} and update properties to match your case"
+    echo
+    exit 1
+fi
+
+python ${BASE_DIR}/oozie_converter.py -i ${BASE_DIR}/examples/decision/workflow.xml \
+  -p ${BASE_DIR}/examples/decision/job.properties \
+  -o ${BASE_DIR}/output/decision_test.py -c ${BASE_DIR}/examples/decision/configuration.properties -d test_decision_dag $@
+
+gsutil cp ${BASE_DIR}/output/decision_test.py gs://europe-west1-o2a-integratio-f690ede2-bucket/dags/
+
+# O2A libs
+gsutil cp -r ${BASE_DIR}/o2a_libs gs://europe-west1-o2a-integratio-f690ede2-bucket/dags/
+
+gcloud composer environments run o2a-integration --location europe-west1 list_dags
+
+gcloud composer environments run o2a-integration --location europe-west1 trigger_dag -- test_decision_dag
--- oozie-to-airflow/templates/decision.tpl
+++ oozie-to-airflow/templates/decision.tpl
@@ -2,17 +2,18 @@ def {{ task_id }}_decision():
 {%- for key, val in case_dict -%}
 {%- if loop.first %} 
     if {{ key }}:
-        return {{ val }}
-{% endif %} 
+        return "{{ val }}"
+{% endif %}
 {%- if not loop.first and not loop.last %}
     elif {{ key }}:
-        return {{ val }}
-{% endif %} 
-{%- if loop.last %} 
+        return "{{ val }}"
+{% endif %}
+{%- if loop.last %}
     else:
-        return {{ val }}
-{%- endif %} 
+        return "{{ val }}"
+{%- endif %}
 {%- endfor %}
+
 
 {{ task_id }} = python_operator.BranchPythonOperator(
     python_callable={{task_id}}_decision,
