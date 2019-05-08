ymmah/cloud-composer

Change required_imports() in all mappers from static to an instance method

oozie-to-airflow/tests/mappers/test_end_mapper.py
@@ -30,31 +30,25 @@ class TestEndMapper(unittest.TestCase):
     oozie_node = Element("end")
 
     def test_create_mapper(self):
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
 
     def test_convert_to_text(self):
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = end_mapper.EndMapper.required_imports()
+        mapper = self._get_mapper()
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
 
     def test_on_parse_finish_simple_should_remove_end_node(self):
         workflow = Workflow(input_directory_path=None, output_directory_path=None, dag_name=None)
 
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="second_task", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper("second_task")
 
         workflow.nodes["first_task"] = ParsedNode(mock.Mock(autospec=BaseMapper))
         workflow.nodes["second_task"] = ParsedNode(mapper)
@@ -69,9 +63,7 @@ class TestEndMapper(unittest.TestCase):
     def test_on_parse_finish_decision_should_not_remove_end_node(self):
         workflow = Workflow(input_directory_path=None, output_directory_path=None, dag_name=None)
 
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="second_task", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper("second_task")
 
         workflow.nodes["first_task"] = ParsedNode(mock.Mock(spec=DecisionMapper, last_task_id="first_task"))
         workflow.nodes["second_task"] = ParsedNode(mapper)
@@ -82,3 +74,7 @@ class TestEndMapper(unittest.TestCase):
 
         self.assertEqual(set(workflow.nodes.keys()), {"first_task", "second_task"})
         self.assertEqual(workflow.relations, {Relation(from_task_id="first_task", to_task_id="second_task")})
+
+    def _get_mapper(self, name="test_id"):
+        mapper = end_mapper.EndMapper(oozie_node=self.oozie_node, name=name, trigger_rule=TriggerRule.DUMMY)
+        return mapper

oozie-to-airflow/tests/mappers/test_fs_mapper.py
@@ -164,7 +164,7 @@ class FsMapperSingleTestCase(unittest.TestCase):
             </fs>"""
         self.node = ET.fromstring(node_str)
 
-        self.mapper = fs_mapper.FsMapper(oozie_node=self.node, name="test_id", trigger_rule=TriggerRule.DUMMY)
+        self.mapper = _get_mapper(oozie_node=self.node)
         self.mapper.on_parse_node()
 
     def test_convert_to_text(self):
@@ -172,7 +172,7 @@ class FsMapperSingleTestCase(unittest.TestCase):
         self.assertIsNotNone(ast.parse(self.mapper.convert_to_text()))
 
     def test_required_imports(self):
-        imps = fs_mapper.FsMapper.required_imports()
+        imps = self.mapper.required_imports()
         imp_str = "\n".join(imps)
         self.assertIsNotNone(ast.parse(imp_str))
 
@@ -186,7 +186,7 @@ class FsMapperSingleTestCase(unittest.TestCase):
 class FsMapperEmptyTestCase(unittest.TestCase):
     def setUp(self):
         self.node = ET.Element("fs")
-        self.mapper = fs_mapper.FsMapper(oozie_node=self.node, name="test_id", trigger_rule=TriggerRule.DUMMY)
+        self.mapper = _get_mapper(oozie_node=self.node)
         self.mapper.on_parse_node()
 
     def test_convert_to_text(self):
@@ -194,7 +194,7 @@ class FsMapperEmptyTestCase(unittest.TestCase):
         self.assertIsNotNone(ast.parse(self.mapper.convert_to_text()))
 
     def test_required_imports(self):
-        imps = fs_mapper.FsMapper.required_imports()
+        imps = self.mapper.required_imports()
         imp_str = "\n".join(imps)
         self.assertIsNotNone(ast.parse(imp_str))
 
@@ -247,7 +247,7 @@ class FsMapperComplexTestCase(unittest.TestCase):
             </fs>"""
         self.node = ET.fromstring(node_str)
 
-        self.mapper = fs_mapper.FsMapper(oozie_node=self.node, name="test_id", trigger_rule=TriggerRule.DUMMY)
+        self.mapper = _get_mapper(oozie_node=self.node)
         self.mapper.on_parse_node()
 
     def test_convert_to_text(self):
@@ -255,7 +255,7 @@ class FsMapperComplexTestCase(unittest.TestCase):
         self.assertIsNotNone(ast.parse(self.mapper.convert_to_text()))
 
     def test_required_imports(self):
-        imps = fs_mapper.FsMapper.required_imports()
+        imps = self.mapper.required_imports()
         imp_str = "\n".join(imps)
         self.assertIsNotNone(ast.parse(imp_str))
 
@@ -264,3 +264,7 @@ class FsMapperComplexTestCase(unittest.TestCase):
 
     def test_get_last_task_id(self):
         self.assertEqual(self.mapper.last_task_id, "test_id_fs_17_chgrp")
+
+
+def _get_mapper(oozie_node):
+    return fs_mapper.FsMapper(oozie_node=oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY)

oozie-to-airflow/tests/mappers/test_kill_mapper.py
@@ -30,32 +30,20 @@ class TestKillMapper(unittest.TestCase):
     oozie_node = Element("dummy")
 
     def test_create_mapper(self):
-        mapper = kill_mapper.KillMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
 
     def test_convert_to_text(self):
-        mapper = kill_mapper.KillMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
-
-    # pylint: disable=no-self-use
-    def test_required_imports(self):
-        imps = kill_mapper.KillMapper.required_imports()
-        imp_str = "\n".join(imps)
-        ast.parse(imp_str)
 
     def test_on_parse_finish(self):
         workflow = Workflow(input_directory_path=None, output_directory_path=None, dag_name=None)
 
-        mapper = kill_mapper.KillMapper(
-            oozie_node=self.oozie_node, name="fail_task", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper(name="fail_task")
 
         workflow.nodes["task"] = ParsedNode(mock.Mock(autospec=BaseMapper))
         workflow.nodes["fail_task"] = ParsedNode(mapper)
@@ -72,3 +60,12 @@ class TestKillMapper(unittest.TestCase):
 
         self.assertEqual(set(workflow.nodes.keys()), {"task", "success_task"})
         self.assertEqual(workflow.relations, {Relation(from_task_id="task", to_task_id="success_task")})
+
+    def test_required_imports(self):
+        mapper = self._get_mapper()
+        imps = mapper.required_imports()
+        imp_str = "\n".join(imps)
+        ast.parse(imp_str)
+
+    def _get_mapper(self, name="test_id"):
+        return kill_mapper.KillMapper(oozie_node=self.oozie_node, name=name, trigger_rule=TriggerRule.DUMMY)

oozie-to-airflow/tests/mappers/test_mapreduce_mapper.py
@@ -70,9 +70,7 @@ class TestMapReduceMapper(unittest.TestCase):
         self.mapreduce_node = ET.fromstring(mapreduce_node_str)
 
     def test_create_mapper_no_jinja(self):
-        mapper = mapreduce_mapper.MapReduceMapper(
-            oozie_node=self.mapreduce_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -97,9 +95,7 @@ class TestMapReduceMapper(unittest.TestCase):
         # test jinja templating
         params = {"nameNode": "hdfs://", "queueName": "myQueue", "examplesRoot": "examples"}
 
-        mapper = mapreduce_mapper.MapReduceMapper(
-            oozie_node=self.mapreduce_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
-        )
+        mapper = self._get_mapper(params=params)
 
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
@@ -134,8 +130,13 @@ class TestMapReduceMapper(unittest.TestCase):
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = mapreduce_mapper.MapReduceMapper.required_imports()
+        mapper = self._get_mapper()
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
+
+    def _get_mapper(self, params=None):
+        return mapreduce_mapper.MapReduceMapper(
+            oozie_node=self.mapreduce_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
+        )

oozie-to-airflow/tests/mappers/test_shell_mapper.py
@@ -52,9 +52,7 @@ class TestShellMapper(unittest.TestCase):
         self.shell_node = ET.fromstring(shell_node_str)
 
     def test_create_mapper_no_jinja(self):
-        mapper = shell_mapper.ShellMapper(
-            oozie_node=self.shell_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper(params=None)
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -75,9 +73,7 @@ class TestShellMapper(unittest.TestCase):
             "examplesRoot": "examples",
         }
 
-        mapper = shell_mapper.ShellMapper(
-            oozie_node=self.shell_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
-        )
+        mapper = self._get_mapper(params=params)
 
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
@@ -89,21 +85,23 @@ class TestShellMapper(unittest.TestCase):
         self.assertEqual("echo arg1 arg2", mapper.bash_command)
 
     def test_convert_to_text(self):
-        mapper = shell_mapper.ShellMapper(
-            oozie_node=self.shell_node,
-            name="test_id",
-            trigger_rule=TriggerRule.DUMMY,
-            params={
-                "dataproc_cluster": "my-cluster",
-                "gcp_region": "europe-west3",
-                "nameNode": "hdfs://localhost:9020/",
-            },
-        )
+        params = {
+            "dataproc_cluster": "my-cluster",
+            "gcp_region": "europe-west3",
+            "nameNode": "hdfs://localhost:9020/",
+        }
+        mapper = self._get_mapper(params=params)
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = shell_mapper.ShellMapper.required_imports()
+        params = {"nameNode": "hdfs://localhost:9020/"}
+        mapper = self._get_mapper(params=params)
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
+
+    def _get_mapper(self, params):
+        return shell_mapper.ShellMapper(
+            oozie_node=self.shell_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
+        )

oozie-to-airflow/tests/mappers/test_ssh_mapper.py
@@ -36,9 +36,7 @@ class TestSSHMapper(unittest.TestCase):
         self.ssh_node = ET.fromstring(ssh_node_str)
 
     def test_create_mapper_no_jinja(self):
-        mapper = ssh_mapper.SSHMapper(
-            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -52,9 +50,7 @@ class TestSSHMapper(unittest.TestCase):
         self.ssh_node.find("host").text = "${hostname}"
         params = {"hostname": "user@apache.org"}
 
-        mapper = ssh_mapper.SSHMapper(
-            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
-        )
+        mapper = self._get_mapper(params=params)
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -64,14 +60,18 @@ class TestSSHMapper(unittest.TestCase):
         self.assertEqual("'ls -l -a'", mapper.command)
 
     def test_convert_to_text(self):
-        mapper = ssh_mapper.SSHMapper(
-            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapper()
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = ssh_mapper.SSHMapper.required_imports()
+        mapper = self._get_mapper()
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
+
+    def _get_mapper(self, params=None):
+        mapper = ssh_mapper.SSHMapper(
+            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
+        )
+        return mapper

GoogleCloudPlatform/oozie-to-airflow

oozie-to-airflow/tests/mappers/test_end_mapper.py
@@ -30,31 +30,25 @@ class TestEndMapper(unittest.TestCase):
     oozie_node = Element("end")
 
     def test_create_mapper(self):
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_end_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
 
     def test_convert_to_text(self):
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_end_mapper()
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = end_mapper.EndMapper.required_imports()
+        mapper = self._get_end_mapper()
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
 
     def test_on_parse_finish_simple_should_remove_end_node(self):
         workflow = Workflow(input_directory_path=None, output_directory_path=None, dag_name=None)
 
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="second_task", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_end_mapper("second_task")
 
         workflow.nodes["first_task"] = ParsedNode(mock.Mock(autospec=BaseMapper))
         workflow.nodes["second_task"] = ParsedNode(mapper)
@@ -69,9 +63,7 @@ class TestEndMapper(unittest.TestCase):
     def test_on_parse_finish_decision_should_not_remove_end_node(self):
         workflow = Workflow(input_directory_path=None, output_directory_path=None, dag_name=None)
 
-        mapper = end_mapper.EndMapper(
-            oozie_node=self.oozie_node, name="end_task", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_end_mapper("end_task")
 
         workflow.nodes["first_task"] = ParsedNode(mock.Mock(spec=DecisionMapper, last_task_id="first_task"))
         workflow.nodes["second_task"] = ParsedNode(mock.Mock(spec=BaseMapper, last_task_id="second_task"))
@@ -86,3 +78,7 @@ class TestEndMapper(unittest.TestCase):
 
         self.assertEqual(set(workflow.nodes.keys()), {"first_task", "second_task", "end_task"})
         self.assertEqual(workflow.relations, {Relation(from_task_id="first_task", to_task_id="end_task")})
+
+    def _get_end_mapper(self, name="test_id"):
+        mapper = end_mapper.EndMapper(oozie_node=self.oozie_node, name=name, trigger_rule=TriggerRule.DUMMY)
+        return mapper

oozie-to-airflow/tests/mappers/test_fs_mapper.py
@@ -164,7 +164,7 @@ class FsMapperSingleTestCase(unittest.TestCase):
             </fs>"""
         self.node = ET.fromstring(node_str)
 
-        self.mapper = fs_mapper.FsMapper(oozie_node=self.node, name="test_id", trigger_rule=TriggerRule.DUMMY)
+        self.mapper = _get_fs_mapper(oozie_node=self.node)
         self.mapper.on_parse_node()
 
     def test_convert_to_text(self):
@@ -172,7 +172,7 @@ class FsMapperSingleTestCase(unittest.TestCase):
         self.assertIsNotNone(ast.parse(self.mapper.convert_to_text()))
 
     def test_required_imports(self):
-        imps = fs_mapper.FsMapper.required_imports()
+        imps = self.mapper.required_imports()
         imp_str = "\n".join(imps)
         self.assertIsNotNone(ast.parse(imp_str))
 
@@ -186,7 +186,7 @@ class FsMapperSingleTestCase(unittest.TestCase):
 class FsMapperEmptyTestCase(unittest.TestCase):
     def setUp(self):
         self.node = ET.Element("fs")
-        self.mapper = fs_mapper.FsMapper(oozie_node=self.node, name="test_id", trigger_rule=TriggerRule.DUMMY)
+        self.mapper = _get_fs_mapper(oozie_node=self.node)
         self.mapper.on_parse_node()
 
     def test_convert_to_text(self):
@@ -194,7 +194,7 @@ class FsMapperEmptyTestCase(unittest.TestCase):
         self.assertIsNotNone(ast.parse(self.mapper.convert_to_text()))
 
     def test_required_imports(self):
-        imps = fs_mapper.FsMapper.required_imports()
+        imps = self.mapper.required_imports()
         imp_str = "\n".join(imps)
         self.assertIsNotNone(ast.parse(imp_str))
 
@@ -247,7 +247,7 @@ class FsMapperComplexTestCase(unittest.TestCase):
             </fs>"""
         self.node = ET.fromstring(node_str)
 
-        self.mapper = fs_mapper.FsMapper(oozie_node=self.node, name="test_id", trigger_rule=TriggerRule.DUMMY)
+        self.mapper = _get_fs_mapper(oozie_node=self.node)
         self.mapper.on_parse_node()
 
     def test_convert_to_text(self):
@@ -255,7 +255,7 @@ class FsMapperComplexTestCase(unittest.TestCase):
         self.assertIsNotNone(ast.parse(self.mapper.convert_to_text()))
 
     def test_required_imports(self):
-        imps = fs_mapper.FsMapper.required_imports()
+        imps = self.mapper.required_imports()
         imp_str = "\n".join(imps)
         self.assertIsNotNone(ast.parse(imp_str))
 
@@ -264,3 +264,7 @@ class FsMapperComplexTestCase(unittest.TestCase):
 
     def test_get_last_task_id(self):
         self.assertEqual(self.mapper.last_task_id, "test_id_fs_17_chgrp")
+
+
+def _get_fs_mapper(oozie_node):
+    return fs_mapper.FsMapper(oozie_node=oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY)

oozie-to-airflow/tests/mappers/test_kill_mapper.py
@@ -30,32 +30,20 @@ class TestKillMapper(unittest.TestCase):
     oozie_node = Element("dummy")
 
     def test_create_mapper(self):
-        mapper = kill_mapper.KillMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_kill_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
 
     def test_convert_to_text(self):
-        mapper = kill_mapper.KillMapper(
-            oozie_node=self.oozie_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_kill_mapper()
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
-
-    # pylint: disable=no-self-use
-    def test_required_imports(self):
-        imps = kill_mapper.KillMapper.required_imports()
-        imp_str = "\n".join(imps)
-        ast.parse(imp_str)
 
     def test_on_parse_finish(self):
         workflow = Workflow(input_directory_path=None, output_directory_path=None, dag_name=None)
 
-        mapper = kill_mapper.KillMapper(
-            oozie_node=self.oozie_node, name="fail_task", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_kill_mapper(name="fail_task")
 
         workflow.nodes["task"] = ParsedNode(mock.Mock(autospec=BaseMapper))
         workflow.nodes["fail_task"] = ParsedNode(mapper)
@@ -72,3 +60,12 @@ class TestKillMapper(unittest.TestCase):
 
         self.assertEqual(set(workflow.nodes.keys()), {"task", "success_task"})
         self.assertEqual(workflow.relations, {Relation(from_task_id="task", to_task_id="success_task")})
+
+    def test_required_imports(self):
+        mapper = self._get_kill_mapper()
+        imps = mapper.required_imports()
+        imp_str = "\n".join(imps)
+        ast.parse(imp_str)
+
+    def _get_kill_mapper(self, name="test_id"):
+        return kill_mapper.KillMapper(oozie_node=self.oozie_node, name=name, trigger_rule=TriggerRule.DUMMY)

oozie-to-airflow/tests/mappers/test_mapreduce_mapper.py
@@ -74,9 +74,7 @@ class TestMapReduceMapper(unittest.TestCase):
         self.mapreduce_node = ET.fromstring(mapreduce_node_str)
 
     def test_create_mapper_no_jinja(self):
-        mapper = mapreduce_mapper.MapReduceMapper(
-            oozie_node=self.mapreduce_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_mapreduce_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -102,9 +100,7 @@ class TestMapReduceMapper(unittest.TestCase):
         # test jinja templating
         params = {"nameNode": "hdfs://", "queueName": "myQueue", "examplesRoot": "examples"}
 
-        mapper = mapreduce_mapper.MapReduceMapper(
-            oozie_node=self.mapreduce_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
-        )
+        mapper = self._get_mapreduce_mapper(params=params)
 
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
@@ -144,8 +140,13 @@ class TestMapReduceMapper(unittest.TestCase):
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = mapreduce_mapper.MapReduceMapper.required_imports()
+        mapper = self._get_mapreduce_mapper()
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
+
+    def _get_mapreduce_mapper(self, params=None):
+        return mapreduce_mapper.MapReduceMapper(
+            oozie_node=self.mapreduce_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
+        )

oozie-to-airflow/tests/mappers/test_shell_mapper.py
@@ -52,9 +52,7 @@ class TestShellMapper(unittest.TestCase):
         self.shell_node = ET.fromstring(shell_node_str)
 
     def test_create_mapper_no_jinja(self):
-        mapper = shell_mapper.ShellMapper(
-            oozie_node=self.shell_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_shell_mapper(params=None)
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -75,9 +73,7 @@ class TestShellMapper(unittest.TestCase):
             "examplesRoot": "examples",
         }
 
-        mapper = shell_mapper.ShellMapper(
-            oozie_node=self.shell_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
-        )
+        mapper = self._get_shell_mapper(params=params)
 
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
@@ -89,21 +85,23 @@ class TestShellMapper(unittest.TestCase):
         self.assertEqual("echo arg1 arg2", mapper.bash_command)
 
     def test_convert_to_text(self):
-        mapper = shell_mapper.ShellMapper(
-            oozie_node=self.shell_node,
-            name="test_id",
-            trigger_rule=TriggerRule.DUMMY,
-            params={
-                "dataproc_cluster": "my-cluster",
-                "gcp_region": "europe-west3",
-                "nameNode": "hdfs://localhost:9020/",
-            },
-        )
+        params = {
+            "dataproc_cluster": "my-cluster",
+            "gcp_region": "europe-west3",
+            "nameNode": "hdfs://localhost:9020/",
+        }
+        mapper = self._get_shell_mapper(params=params)
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = shell_mapper.ShellMapper.required_imports()
+        params = {"nameNode": "hdfs://localhost:9020/"}
+        mapper = self._get_shell_mapper(params=params)
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
+
+    def _get_shell_mapper(self, params):
+        return shell_mapper.ShellMapper(
+            oozie_node=self.shell_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
+        )

oozie-to-airflow/tests/mappers/test_ssh_mapper.py
@@ -36,9 +36,7 @@ class TestSSHMapper(unittest.TestCase):
         self.ssh_node = ET.fromstring(ssh_node_str)
 
     def test_create_mapper_no_jinja(self):
-        mapper = ssh_mapper.SSHMapper(
-            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_ssh_mapper()
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -52,9 +50,7 @@ class TestSSHMapper(unittest.TestCase):
         self.ssh_node.find("host").text = "${hostname}"
         params = {"hostname": "user@apache.org"}
 
-        mapper = ssh_mapper.SSHMapper(
-            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
-        )
+        mapper = self._get_ssh_mapper(params=params)
         # make sure everything is getting initialized correctly
         self.assertEqual("test_id", mapper.name)
         self.assertEqual(TriggerRule.DUMMY, mapper.trigger_rule)
@@ -64,14 +60,18 @@ class TestSSHMapper(unittest.TestCase):
         self.assertEqual("'ls -l -a'", mapper.command)
 
     def test_convert_to_text(self):
-        mapper = ssh_mapper.SSHMapper(
-            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY
-        )
+        mapper = self._get_ssh_mapper()
         # Throws a syntax error if doesn't parse correctly
         ast.parse(mapper.convert_to_text())
 
-    # pylint: disable=no-self-use
     def test_required_imports(self):
-        imps = ssh_mapper.SSHMapper.required_imports()
+        mapper = self._get_ssh_mapper()
+        imps = mapper.required_imports()
         imp_str = "\n".join(imps)
         ast.parse(imp_str)
+
+    def _get_ssh_mapper(self, params=None):
+        mapper = ssh_mapper.SSHMapper(
+            oozie_node=self.ssh_node, name="test_id", trigger_rule=TriggerRule.DUMMY, params=params
+        )
+        return mapper