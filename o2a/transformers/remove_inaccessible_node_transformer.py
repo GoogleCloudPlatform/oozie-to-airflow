deb470110838c1b350045ac5d1442306fbf48791
Googler <noreply@google.com>
2019-06-27 08:10:59 +0200

    Remove inacccessible nodes Close #254
    GitOrigin-RevId: 4942249736a61af9e3860842c0d07e2d9c47f551
    Change-Id: I3585a1a682c88890007ce09efe9b4cc82d18f8f1

---
+++ o2a/transformers/remove_inaccessible_node_transformer.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Remove inaccessible transformer"""
+
+from typing import Dict
+
+from o2a.converter.parsed_action_node import ParsedActionNode
+from o2a.converter.workflow import Workflow
+from o2a.mappers.start_mapper import StartMapper
+from o2a.transformers.base_transformer import BaseWorkflowTransformer
+
+
+# pylint: disable=too-few-public-methods
+class RemoveInaccessibleNodeTransformer(BaseWorkflowTransformer):
+    """
+    Transformer that remove nodes that are not accessible from Start node. In the case of Airflow,
+    all nodes are executed. In the case of Oozie, only nodes that have a connection
+    to Start node are executed.
+    """
+
+    def process_workflow(self, workflow: Workflow):
+        accessible_nodes = self._find_accessible_nodes(workflow)
+
+        for node in workflow.nodes.copy().values():
+            if node not in accessible_nodes:
+                workflow.remove_node(node)
+
+    @staticmethod
+    def _find_accessible_nodes(workflow: Workflow):
+        """
+        Finds nodes that are reachable from any Start node.
+        """
+        start_nodes = workflow.get_nodes_by_type(StartMapper)
+        visited_node: Dict[str, ParsedActionNode] = dict()
+
+        def visit_node(node: ParsedActionNode):
+            if node.name in visited_node:
+                return
+            visited_node[node.name] = node
+
+            all_downstream_node_names = [*node.downstream_names]
+            if node.error_xml:
+                all_downstream_node_names.append(node.error_xml)
+
+            for node_name in all_downstream_node_names:
+                visit_node(workflow.nodes[node_name])
+
+        for start_node in start_nodes:
+            visit_node(start_node)
+
+        return visited_node.values()
---
+++ tests/transformers/test_inaccessible_node_transformer.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Remove inaccessible nodes transformer tests
+"""
+import unittest
+from unittest import mock
+
+from o2a.converter.parsed_action_node import ParsedActionNode
+from o2a.converter.task import Task
+from o2a.converter.workflow import Workflow
+from o2a.mappers.base_mapper import BaseMapper
+from o2a.mappers.start_mapper import StartMapper
+from o2a.transformers.remove_inaccessible_node_transformer import RemoveInaccessibleNodeTransformer
+
+
+class RemoveInaccessibleNodeTransformerTest(unittest.TestCase):
+    def test_should_keep_connected_nodes_in_correct_flow(self):
+        """
+        Graph before:
+
+        .. graphviz::
+
+           digraph foo {
+              S -> A
+              A -> B
+           }
+
+        Graph after:
+
+        .. graphviz::
+
+           digraph foo {
+              S -> A
+              A -> B
+           }
+
+        Where:
+        A - first_task
+        B - second_task
+        S - start_task
+        """
+        transformer = RemoveInaccessibleNodeTransformer()
+
+        workflow = Workflow(input_directory_path="", output_directory_path="", dag_name="DAG_NAME_B")
+
+        first_mapper = mock.Mock(spec=BaseMapper)
+        first_mapper.name = "first_task"
+
+        second_mapper = mock.Mock(spec=BaseMapper)
+        second_mapper.name = "second_task"
+
+        start_mapper = mock.Mock(spec=StartMapper)
+        start_mapper.name = "start_task"
+
+        first_node = ParsedActionNode(mapper=first_mapper, tasks=[self._get_dummy_task(first_mapper.name)])
+        second_node = ParsedActionNode(mapper=second_mapper, tasks=[self._get_dummy_task(second_mapper.name)])
+        start_node = ParsedActionNode(mapper=start_mapper, tasks=[self._get_dummy_task(start_mapper.name)])
+
+        start_node.downstream_names = [first_mapper.name]
+        first_node.downstream_names = [second_node.name]
+
+        workflow.nodes[first_mapper.name] = first_node
+        workflow.nodes[second_mapper.name] = second_node
+        workflow.nodes[start_mapper.name] = start_node
+
+        transformer.process_workflow(workflow)
+
+        self.assertEqual(
+            {start_mapper.name, first_mapper.name, second_mapper.name}, set(workflow.nodes.keys())
+        )
+        self.assertEqual([first_mapper.name], start_node.downstream_names)
+        self.assertEqual([second_mapper.name], first_node.downstream_names)
+        self.assertEqual([], second_node.downstream_names)
+
+    def test_should_keep_connected_nodes_in_error_state(self):
+        """
+        Graph before:
+
+        .. graphviz::
+
+           digraph foo {
+              S -> A
+           }
+
+        Graph after:
+
+        .. graphviz::
+
+           digraph foo {
+              S -> A
+           }
+
+        Where:
+        A - first_task
+        S - start_task
+        """
+        transformer = RemoveInaccessibleNodeTransformer()
+
+        workflow = Workflow(input_directory_path="", output_directory_path="", dag_name="DAG_NAME_B")
+
+        first_mapper = mock.Mock(spec=BaseMapper)
+        first_mapper.name = "first_task"
+
+        start_mapper = mock.Mock(spec=StartMapper)
+        start_mapper.name = "start_task"
+
+        first_node = ParsedActionNode(mapper=first_mapper, tasks=[self._get_dummy_task(first_mapper.name)])
+        start_node = ParsedActionNode(mapper=start_mapper, tasks=[self._get_dummy_task(start_mapper.name)])
+
+        start_node.error_xml = first_node.name
+
+        workflow.nodes[first_mapper.name] = first_node
+        workflow.nodes[start_mapper.name] = start_node
+
+        transformer.process_workflow(workflow)
+
+        self.assertEqual({start_mapper.name, first_mapper.name}, set(workflow.nodes.keys()))
+        self.assertEqual([], start_node.downstream_names)
+        self.assertEqual([], first_node.downstream_names)
+        self.assertEqual(first_mapper.name, start_node.error_xml)
+        self.assertEqual(None, first_node.error_xml)
+
+    def test_should_remove_inaccessible_node(self):
+        """
+        Graph before:
+
+        .. graphviz::
+
+           digraph foo {
+              S -> A
+              B -> A
+           }
+
+        Graph after:
+
+        .. graphviz::
+
+           digraph foo {
+              S -> A
+           }
+
+        Where:
+        A - first_task
+        B - second_task
+        S - start_task
+        """
+        transformer = RemoveInaccessibleNodeTransformer()
+
+        workflow = Workflow(input_directory_path="", output_directory_path="", dag_name="DAG_NAME_B")
+
+        first_mapper = mock.Mock(spec=BaseMapper)
+        first_mapper.name = "first_task"
+
+        second_mapper = mock.Mock(spec=BaseMapper)
+        second_mapper.name = "second_task"
+
+        start_mapper = mock.Mock(spec=StartMapper)
+        start_mapper.name = "start_task"
+
+        first_node = ParsedActionNode(mapper=first_mapper, tasks=[self._get_dummy_task(first_mapper.name)])
+        second_node = ParsedActionNode(mapper=second_mapper, tasks=[self._get_dummy_task(second_mapper.name)])
+        start_node = ParsedActionNode(mapper=start_mapper, tasks=[self._get_dummy_task(start_mapper.name)])
+
+        first_node.downstream_names = []
+        second_node.downstream_names = [first_mapper.name]
+        start_node.downstream_names = [first_mapper.name]
+
+        workflow.nodes[first_mapper.name] = first_node
+        workflow.nodes[second_mapper.name] = second_node
+        workflow.nodes[start_mapper.name] = start_node
+        transformer.process_workflow(workflow)
+
+        self.assertEqual({start_mapper.name, first_mapper.name}, set(workflow.nodes.keys()))
+        self.assertEqual([first_node.name], start_node.downstream_names)
+
+    def test_should_remove_group_of_inaccessible_nodes(self):
+        """
+        Graph before:
+
+        .. graphviz::
+
+            digraph foo {
+                A -> B
+                B -> S
+            }
+
+        Graph after:
+
+        .. graphviz::
+
+            digraph foo {
+                S
+            }
+
+        Where:
+        A - first_task
+        B - second_task
+        S - start_task
+        """
+        transformer = RemoveInaccessibleNodeTransformer()
+
+        workflow = Workflow(input_directory_path="", output_directory_path="", dag_name="DAG_NAME_B")
+
+        first_mapper = mock.Mock(spec=BaseMapper)
+        first_mapper.name = "first_task"
+
+        second_mapper = mock.Mock(spec=BaseMapper)
+        second_mapper.name = "second_task"
+
+        start_mapper = mock.Mock(spec=StartMapper)
+        start_mapper.name = "start_task"
+
+        first_node = ParsedActionNode(mapper=first_mapper, tasks=[self._get_dummy_task(first_mapper.name)])
+        second_node = ParsedActionNode(mapper=second_mapper, tasks=[self._get_dummy_task(second_mapper.name)])
+        start_node = ParsedActionNode(mapper=start_mapper, tasks=[self._get_dummy_task(start_mapper.name)])
+
+        first_mapper.downstream_names = [second_mapper.name]
+        second_mapper.downstream_names = [start_mapper.name]
+
+        workflow.nodes[first_mapper.name] = first_node
+        workflow.nodes[second_mapper.name] = second_node
+        workflow.nodes[start_mapper.name] = start_node
+
+        transformer.process_workflow(workflow)
+
+        self.assertEqual({start_mapper.name}, set(workflow.nodes.keys()))
+        self.assertEqual([second_mapper.name], first_mapper.downstream_names)
+        self.assertEqual([start_mapper.name], second_mapper.downstream_names)
+
+    @staticmethod
+    def _get_dummy_task(task_id):
+        return Task(task_id=task_id, template_name="dummy.tpl")
