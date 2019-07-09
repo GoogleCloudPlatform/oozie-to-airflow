15e2ae2e45500b084044ae433e9cb9583b6b7e25
Tomek <tomasz.urbaszek@polidea.com>
2019-07-09 18:22:15 +0200

    Improve parsing of el functions (#261)
    * Improve parsing of el functions

--- o2a/o2a_libs/el_wf_functions.py
+++ o2a/o2a_libs/el_wf_functions.py
@@ -13,30 +13,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """All WF EL functions"""
-# pylint: disable=unused-argument
+
+from typing import Optional
+
+from jinja2 import contextfunction
+
+from airflow.models import TaskInstance, DagRun
+from airflow.utils.db import provide_session
+from airflow import AirflowException
 
 
-def wf_id():
+def _reverse_task_map(task_map: dict) -> dict:
     """
-    It returns the workflow job ID for the current workflow job.
+    Given a map {oozie_node: [airflow_node1, airflow_node2]} it returns
+    reversed map {airflow_node1: oozie_node, airflow_node2: oozie_node}.
 
-    In airflow it can be found using Jinja templating for `run_id`
+    :param task_map: oozie to airflow task map
+    :return: reversed task map
+    """
+    new_map = dict()
+    for oozie_node, airflow_tasks in task_map.items():
+        new_map.update({t: oozie_node for t in airflow_tasks})
+    return new_map
+
+
+def conf(key: str) -> str:
+    """
+    It returns the value of the workflow job configuration property for the
+    current workflow job, or an empty string if undefined.
+
     This has the effect that some parameters cannot be templated, and thus
     this will fail.
     """
-    return "{{ run_id }}"
+    return "params[{}]".format(key)
 
 
-def wf_name():
+@contextfunction
+@provide_session
+def last_error_node(context=None, session=None) -> str:
     """
-    It returns the workflow application name for the current workflow job.
-
-    This has the effect that some parameters cannot be templated, and thus
-    this will fail.
-
-    :return: Current DAG id.
+    It returns the name of the last workflow action node that exit with an ERROR
+    exit state, or an empty string if no action has exited with ERROR state in the
+    current workflow job.
     """
-    return "{{ dag.dag_id }}"
+    drun: Optional[DagRun] = context.get("dag_run", None)
+    if drun is None:
+        raise AirflowException("No dag_run reference in context.")
+
+    dag_id = drun.dag_id
+
+    ti = TaskInstance  # pylint:disable=invalid-name
+    last_failed_task = (
+        session.query(TaskInstance)
+        .filter(ti.dag_id == dag_id)
+        .filter(ti.task_id.endswith("_error"))
+        .order_by(ti.execution_date.asc())
+        .first()
+    )
+
+    if not last_failed_task:
+        return ""
+
+    task_name: str = last_failed_task.task_id
+
+    task_map: Optional[dict] = context.get("task_map", None)
+    if task_map is None:
+        raise AirflowException("No task map!")
+
+    reversed_map: dict = _reverse_task_map(task_map)
+    return reversed_map.get(task_name, "")
 
 
 def wf_app_path():
@@ -47,22 +92,10 @@ def wf_app_path():
     """
 
 
-def wf_conf():
+def wf_conf(key):  # pylint: disable=unused-argument
     """
-    It returns the value of the workflow job configuration property for the
-    current workflow job, or an empty string if undefined.
-
-    This has the effect that some parameters cannot be templated, and thus
-    this will fail.
+    Todo: Remove when new translations will be used
     """
-
-
-def wf_user():
-    """
-    Returns the user name that started the current workflow job.
-
-    """
-    return "{{ params.props.merged.user.name }}"
 
 
 def wf_group():
@@ -73,7 +106,7 @@ def wf_group():
     """
 
 
-def wf_callback(state_variable):
+def wf_callback(state_variable):  # pylint: disable=unused-argument
     """
     It returns the callback URL for the current workflow action node, stateVar
     can be a valid exit state (=OK= or ERROR ) for the action or a token to be
@@ -90,7 +123,7 @@ def wf_callback(state_variable):
     """
 
 
-def wf_transition(node):
+def wf_transition(node):  # pylint: disable=unused-argument
     """
     It returns the transition taken by the specified workflow action node, or
     an empty string if the action has not being executed or it has not completed
@@ -100,13 +133,11 @@ def wf_transition(node):
 
 def wf_last_error_node():
     """
-    It returns the name of the last workflow action node that exit with an ERROR
-    exit state, or an empty string if no action has exited with ERROR state in the
-    current workflow job.
+    Todo: Remove when new translations will be used
     """
 
 
-def wf_error_code(node):
+def wf_error_code(node):  # pylint: disable=unused-argument
     """
     It returns the error code for the specified action node, or an empty string if
     the action node has not exited with ERROR state.
@@ -115,7 +146,7 @@ def wf_error_code(node):
     """
 
 
-def wf_error_message(message):
+def wf_error_message(message):  # pylint: disable=unused-argument
     """
     It returns the error message for the specified action node, or an empty string
     if no action node has not exited with ERROR state.
@@ -131,7 +162,7 @@ def wf_run():
     """
 
 
-def wf_action_data(node):
+def wf_action_data(node):  # pylint: disable=unused-argument
     """
     This function is only applicable to action nodes that produce output data on
     completion.
@@ -141,21 +172,21 @@ def wf_action_data(node):
     """
 
 
-def wf_action_external_id(node):
+def wf_action_external_id(node):  # pylint: disable=unused-argument
     """
     It returns the external Id for an action node, or an empty string if the
     action has not being executed or it has not completed yet.
     """
 
 
-def wf_action_tracker_uri(node):
+def wf_action_tracker_uri(node):  # pylint: disable=unused-argument
     """
     It returns the tracker URI for an action node, or an empty string if the action
     has not being executed or it has not completed yet.
     """
 
 
-def wf_action_external_status(node):
+def wf_action_external_status(node):  # pylint: disable=unused-argument
     """
     It returns the external status for an action node, or an empty string if the
     action has not being executed or it has not completed yet.
